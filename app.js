0b24512bd6831b17fe4bd0d1df0ef5a2fa89010f
henry <henrygranados03@gmail.com>
2015-10-17 15:16:07 -0700

    update app.js file

--- app.js
+++ app.js
@@ -1,40 +1,49 @@
-//Author: Henry Granados
-//Website: henrygranados.github.io
+$(document).ready(function(){
+
+$(document).mousemove(function(event){ 
+        $("span").text("X: " + ctx.event.pageX + ", Y: " + ctx.event.pageY); 
+});
+$("#test").hide(2000).show(2000);
+
+
 function initCanvas(){
     var ctx = document.getElementById('my_canvas').getContext('2d');
     var myImage = new Image();
     var img  = new Image();
-    var enemies = new Image();
+    var enemiespic = new Image();
     myImage.src = "images/background-pic.jpg"; //Background picture
     img.src = "images/spaceship-pic.png"; //Spaceship picture
-    enemies.src = "images/enemies.png";
+    enemiespic.src = "images/enemies.png"; //Enemies picture
     ctx.drawImage(myImage, 20, 20);
     ctx.drawImage(img,this.x,this.y, 10, 10);
-    ctx.drawImage(enemies, 20, 20);
-    var cW = ctx.canvas.width, cH = ctx.canvas.height;
+    ctx.drawImage(enemiespic, 20, 20);
+    var cW = ctx.canvas.width 
+    var cH = ctx.canvas.height;
     var enemies = [ {"id":"enemy1","x":100,"y":-20,"w":50,"h":30},
                     {"id":"enemy2","x":225,"y":-20,"w":50,"h":30},
-                    {"id":"enemy3","x":350,"y":-20,"w":50,"h":30},
-                    {"id":"enemy4","x":100,"y":-70,"w":50,"h":30},
+                    {"id":"enemy3","x":350,"y":-20,"w":80,"h":50},
+                    {"id":"enemy4","x":100,"y":-70,"w":80,"h":50},
                     {"id":"enemy5","x":225,"y":-70,"w":50,"h":30},
                     {"id":"enemy6","x":350,"y":-70,"w":50,"h":30},
                     {"id":"enemy7","x":475,"y":-70,"w":50,"h":30},
-                    {"id":"enemy8","x":600,"y":-70,"w":50,"h":30},
+                    {"id":"enemy8","x":600,"y":-70,"w":80,"h":50},
                     {"id":"enemy9","x":475,"y":-20,"w":50,"h":30},
                    {"id":"enemy10","x":600,"y":-20,"w":50,"h":30}
 
     ];
     function renderEnemies(){
         for(var i = 0; i < enemies.length; i++){
-            ctx.fillStyle = "orange";
-            ctx.fillRect(enemies[i].x, enemies[i].y+=.5, enemies[i].w, enemies[i].h);
+            //ctx.fillStyle = "green";
+            ctx.drawImage(enemiespic,enemies[i].x,enemies[i].y+=.5, enemies[i].w, enemies[i].h);
+            //ctx.fillRect(enemies[i].x, enemies[i].y+=.5, enemies[i].w, enemies[i].h);
         }
     }
+
     function Launcher(){
         this.y = 500, 
         this.x = cW*.5-25, 
         this.w = 100, 
-        this.h = 100, 
+        this.h = 100,   
         this.dir, 
         this.bg="red", 
         this.missiles = [];
@@ -44,15 +53,16 @@ function initCanvas(){
                 this.x-=5;
             } else if(this.dir == 'right'){
                 this.x+=5;
-            }else if(this.dir == "upArrow"){
-                this.y+=5;
             }else if(this.dir == "downArrow"){
+                this.y+=5;
+            }else if(this.dir == "upArrow"){
                 this.y-=5;
             }
             ctx.fillStyle = this.bg;
             ctx.drawImage(myImage, 10, 10);
             ctx.drawImage(img,this.x,this.y, 100, 90);
             //ctx.fillRect(this.x, this.y, this.w, this.h);
+
             for(var i=0; i < this.missiles.length; i++){
                 var m = this.missiles[i];
                 ctx.fillStyle = ctx.drawImage(img,this.x,this.y, 100, 90);
@@ -73,7 +83,10 @@ function initCanvas(){
         this.hitDetect = function(m,mi){
             for(var i = 0; i < enemies.length; i++){
                 var e = enemies[i];
-                if(m.x+m.w >= e.x && m.x <= e.x+e.w && m.y >= e.y && m.y <= e.y+e.h){
+                if(m.x+m.w >= e.x && 
+                   m.x <= e.x+e.w && 
+                   m.y >= e.y && 
+                   m.y <= e.y+e.h){
                     this.missiles.splice(this.missiles[mi],1); // Remove the missile
                     enemies.splice(i,1); // Remove the enemy that the missile hit
                     document.getElementById('status').innerHTML = "Destroyed "+ e.id+ " ";
@@ -83,28 +96,34 @@ function initCanvas(){
     }
     var launcher = new Launcher();
     function animate(){
-        //ctx.save();
         ctx.clearRect(0, 0, cW, cH);
         launcher.render();
         renderEnemies();
-        //ctx.restore();
     }
     var animateInterval = setInterval(animate, 12);
     var left_btn = document.getElementById('left_btn');
     var right_btn = document.getElementById('right_btn');
     var fire_btn = document.getElementById('fire_btn'); 
+
+
     left_btn.addEventListener('mousedown', function(event) {
         launcher.dir = 'left';
     });
    document.addEventListener('keydown', function(event) {
         if(event.keyCode == 37)
         {
-         launcher.dir = 'left';
-        }
+         launcher.dir = 'left';  
+        if(launcher.x < cW*.2-130){
+            launcher.x+=0;
+            launcher.dir = '';
+         }
+       }
+        
     }, false);
     document.addEventListener('keyup', function(event) {
         if(event.keyCode == 37)
         {
+         launcher.x+=0;
          launcher.dir = '';
         }
     }, false); 
@@ -112,35 +131,52 @@ function initCanvas(){
         if(event.keyCode == 39)
         {
          launcher.dir = 'right';
+         if(launcher.x > cW-110){
+            launcher.x-=0;
+            launcher.dir = '';
+         }
+        
         }
     }, false);
     document.addEventListener('keyup', function(event) {
         if(event.keyCode == 39)
         {
+         launcher.x-=0;   
          launcher.dir = '';
         }
     }, false); 
     document.addEventListener('keydown', function(event){
          if(event.keyCode == 38)
          {
-           launcher.dir = 'downArrow';
+           launcher.dir = 'upArrow';  
+           if(launcher.y < cH*.2-80){
+            launcher.y += 0;
+            launcher.dir = '';
+         }
          }
     }, false);
     document.addEventListener('keyup', function(event){
          if(event.keyCode == 38)
          {
+           launcher.y -= 0;
            launcher.dir = '';
          }
     }, false);
     document.addEventListener('keydown', function(event){
          if(event.keyCode == 40)
          {
-           launcher.dir = 'upArrow';
+           launcher.dir = 'downArrow';  
+          if(launcher.y > cH - 110){
+            //launcher.y += 20;
+            launcher.y -= 0;
+            launcher.dir = '';
+           }
          }
     }, false);
     document.addEventListener('keyup', function(event){
          if(event.keyCode == 40)
          {
+           launcher.y += 0;
            launcher.dir = '';
          }
     }, false);
@@ -170,4 +206,5 @@ function initCanvas(){
 }
 window.addEventListener('load', function(event) {
     initCanvas();
+});
 });
